c5662ec64abcb189d199c0ff62e5830660d878b4
C0ncatS <ahmedselo2005@gmail.com>
2025-09-01 13:37:02 +0300

    Add payment completion task and integrate with Stripe webhook
    - Created a new task in payments/tasks.py to generate and send an invoice PDF via email upon payment completion.
    - Integrated the payment_completed task into the Stripe webhook handler in payments/webhooks.py to trigger invoice generation after successful payment processing.

--- payments/webhooks.py
+++ payments/webhooks.py
@@ -5,6 +5,7 @@ from django.views.decorators.csrf import csrf_exempt
 import stripe.error
 from orders.models import Order
 
+from .tasks import payment_completed
 
 @csrf_exempt
 def stripe_webhook(request):
@@ -34,4 +35,5 @@ def stripe_webhook(request):
             # store Stripe payment ID
             order.stripe_id = session.payment_intent
             order.save()
+            payment_completed.delay(order.id)
     return HttpResponse(status=200)
